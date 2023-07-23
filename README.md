a16e8fad96334d854d5274fc2b94ec58fd022acf
Sparklingbae <sparklingasuquo4142@gmail.com>
2023-07-23 14:45:03 -0700

    printf

---
+++ README.md
@@ -0,0 +1,2 @@
+this is a printf group project done by me and my partner 
+blessing asuquo and suleiman bunawa
---
+++ _printf.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * _print - Prints an argument
+ * @fmt: Formatted string to print the arguments.
+ * @list: List of arguments to be printed
+ * @flags: Calculate active flags.
+ * @precision: Precision specification
+ * @size: Size specifier accordingly
+ * Return: Printed char buffer
+ */
+int _print(const char *fmt, va_list list,
+	int flags, int precision, int size)
+{
+	int i, unknow_len = 0, printed_chars = -1;
+	fmt_t fmt_types[] = {
+		{'c', print_char}, {'s', print_string}, {'%', print_percent},
+		{'i', print_int}, {'d', print_int}, {'b', print_binary},
+		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
+		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
+		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
+	};
+	for (i = 0; fmt_types[i].fmt != '\0'; i++)
+		if (fmt[*ind] == fmt_types[i].fmt)
+			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+
+	if (fmt_types[i].fmt == '\0')
+	{
+		if (fmt[*ind] == '\0')
+			return (-1);
+		unknow_len += write(1, "%%", 1);
+		if (fmt[*ind - 1] == ' ')
+			unknow_len += write(1, " ", 1);
+		else if (width)
+		{
+			--(*ind);
+			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
+				--(*ind);
+			if (fmt[*ind] == ' ')
+				--(*ind);
+			return (1);
+		}
+		unknow_len += write(1, &fmt[*ind], 1);
+		return (unknow_len);
+	}
+	return (printed_chars);
+}
